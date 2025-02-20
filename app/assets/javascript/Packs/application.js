4873f07893313b94a5230d85805b28001ec12d48
karthiknaidu <madalakarthiknaidu3@gmail.com>
2025-02-20 17:37:02 +0530

    improve

---
+++ app/assets/javascript/Packs/application.js
@@ -0,0 +1,9 @@
+// filepath: /D:/Ruby_projects/friends/app/javascript/packs/application.js
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
